8fc814998fcc39f3c3871d024b1a8533d2388e2c
Nuriddin <nuriddin@mst.uz>
2022-06-23 21:10:21 +0500

    Add solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	for _, r := range input {
+		output = string(r) + output
+	}
+
 	return output
 }
